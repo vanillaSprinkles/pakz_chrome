1d01bac0ee5acefbdb0acff1b9251f7f08048d8c
vanillaSprinkles <vanilla.sprinkles@icescreamer.net>
2014-02-02 23:22:22 -0800

    new file:   iron_31.0.1700.0_linux/resources_pak/mod_background_100pct/770 	new file:   iron_31.0.1700.0_linux/resources_pak/mod_background_100pct/771 	new file:   iron_31.0.1700.0_linux/resources_pak/mod_background_100pct/773 	new file:   iron_31.0.1700.0_linux/resources_pak/resources.pak_100pct 	modified:   mkdirz.sh

--- mkdirz.sh
+++ mkdirz.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+BASE_PAK_FILE="resources.pak"
+
+
 [[ -z ${1} ]] && echo "need new folder name" && exit;
 
 [[ -d ${1} ]] && echo "folder '${1}' exists";
@@ -7,16 +10,16 @@
 mkdir -vp ${1}/chrome_pak/{mod_background_contain,mod_background_cover,mod_footer,screenshots,stock}
 
 
-echo "copy /opt/iron/chrome.pak?  y / n"
+echo "copy /opt/iron/${BASE_PAK_FILE}?  y / n"
 read ans
-[[ $ans == "y" ]] && cp /opt/iron/chrome.pak ${1}/chrome_pak/chrome.pak_stock
+[[ $ans == "y" ]] && cp /opt/iron/${BASE_PAK_FILE} ${1}/chrome_pak/${BASE_PAK_FILE}_stock
 
-echo touch ${1}/chrome_pak/chrome.pak_contain
-echo touch ${1}/chrome_pak/chrome.pak_cover
-echo touch ${1}/chrome_pak/chrome.pak_stock
+echo touch ${1}/${BASE_PAK_FILE//./_}/${BASE_PAK_FILE}_contain
+echo touch ${1}/${BASE_PAK_FILE//./_}/${BASE_PAK_FILE}_cover
+echo touch ${1}/${BASE_PAK_FILE//./_}/${BASE_PAK_FILE}_stock
 
-touch ${1}/chrome_pak/chrome.pak_contain
-touch ${1}/chrome_pak/chrome.pak_cover
-touch ${1}/chrome_pak/chrome.pak_stock
+touch ${1}/${BASE_PAK_FILE//./_}/${BASE_PAK_FILE}_contain
+touch ${1}/${BASE_PAK_FILE//./_}/${BASE_PAK_FILE}_cover
+touch ${1}/${BASE_PAK_FILE//./_}/${BASE_PAK_FILE}_stock
 
 
